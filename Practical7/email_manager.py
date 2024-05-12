71336f00efa7a1b7361cb36bfa9779aea6eb1626
Yashvitha <kyashvitha@gmail.com>
2024-05-12 11:58:25 +0200

    feat(email_manager): add send emails function

--- Practical7/email_manager.py
+++ Practical7/email_manager.py
@@ -74,3 +74,89 @@ def get_emails():
     print(recv_quit.decode())
 
     clientSocket.close()
+
+# send emails function
+def send_email():
+    # Prompt user for email details
+    receiver = input("Receiver's email: ")
+    subject = input("Subject: ")
+    message = input("Message: ")
+    bcc = input("BCC: ")
+
+    # Email sender details
+    sender = MY_EMAIL
+    mailserver = ("sandbox.smtp.mailtrap.io", 2525)
+
+    # Create client socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(mailserver)
+
+    # Authenticate with base64 encoding
+    base64_str = ("\x00"+USERNAME+"\x00"+PASSWORD).encode()
+    base64_str = base64.b64encode(base64_str)
+    auth_msg = "AUTH PLAIN ".encode()+base64_str+"\r\n".encode()
+    client_socket.send(auth_msg)
+    recv_auth = client_socket.recv(1024)
+    print(recv_auth.decode())
+
+    # Message to send
+    mail_from = f"MAIL FROM: <{sender}>\r\n"
+    client_socket.send(mail_from.encode())
+    recv_mail_from = client_socket.recv(1024)
+    print("After MAIL FROM command: "+recv_mail_from.decode())
+
+    # Send RCPT TO command
+    rcpt_to = f"RCPT TO: <{receiver}>\r\n"
+    client_socket.send(rcpt_to.encode())
+    recv_rcpt_to = client_socket.recv(1024)
+    print("Server: "+recv_rcpt_to.decode())
+
+    # Send DATA command
+    data = "DATA\r\n"
+    client_socket.send(data.encode())
+    print("Sent DATA command")
+    recv_data = client_socket.recv(1024)
+    print("Server: "+recv_data.decode())
+
+    # Make email header
+    to = f"To: {receiver}\r\n"
+    fr = f"From: {sender}\r\n"
+    bcc = f"Bcc: {bcc}\r\n"
+    subject_header = f"Subject: {subject}\r\n"
+    date = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+    date_header = "Date: " + date + "\r\n\r\n"
+
+    # Construct email
+    email = to + fr + bcc + subject_header + date_header + message + "\r\n.\r\n"
+
+    # Send email
+    client_socket.send(email.encode())
+    recv_email = client_socket.recv(1024)
+    print("Server: "+recv_email.decode())
+
+    # Quit
+    quit_cmd = "QUIT\r\n"
+    client_socket.send(quit_cmd.encode())
+    recv_quit = client_socket.recv(1024)
+    print(recv_quit.decode())
+
+    # Close connection
+    client_socket.close()
+
+    print("Email sent successfully!")
+
+# Main function
+def main():
+    while True:
+        user_input = input("What would you like to do? (get emails, send email, exit): ")
+        if user_input == "get emails":
+            get_emails()
+        elif user_input == "send email":
+            send_email()
+        elif user_input == "exit":
+            break
+        else:
+            print("Invalid input")
+
+if __name__ == "__main__":
+    main()
